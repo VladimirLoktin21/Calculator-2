1e533ca8cfbaa11fbd93e4d4beee850c7653c19d
VladimirLoktin21 <142094719+VladimirLoktin21@users.noreply.github.com>
2024-03-23 10:05:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 def check_answers(answer1, answer2, answer3):
-    correct_answer1 = "CO2"
-    correct_answer2 = "ExtremeWeather"
-    correct_answer3 = "ReducingEmissions"
+    correct_answer1 = "ExtremeWeather"
+    correct_answer2 = "ReducingEmissions"
+    correct_answer3 = "CO2"
 
     score = 0
     if answer1 == correct_answer1:
@@ -21,16 +21,23 @@ def check_answers(answer1, answer2, answer3):
 def index():
     return render_template('global_warming_questions.html')
 
-@app.route('/<question>/<answer>')
-def next_question(question, answer):
-    if question == '1':
-        return render_template('question1.html', answer1=answer)
-    elif question == '2':
-        return render_template('question2.html', size=request.args.get('size'), answer1=request.args.get('answer1'), answer2=answer)
-    elif question == '3':
-        return render_template('question3.html', answer1=request.args.get('answer1'), answer2=request.args.get('answer2'), answer3=answer)
+@app.route('/quiz')
+def question1():
+        return render_template('question1.html')
 
-@app.route('/result/<answer1>/<answer2>/<answer3>')
+@app.route('/quiz/<answer1>')
+def question2(answer1):
+        return render_template('question2.html', 
+                               answer1=answer1)
+
+@app.route('/quiz/<answer1>/<answer2>')
+def question3(answer1, answer2):
+        return render_template('question3.html',
+                               answer1=answer1,
+                               answer2=answer2)
+
+
+@app.route('/quiz/<answer1>/<answer2>/<answer3>')
 def show_result(answer1, answer2, answer3):
     score = check_answers(answer1, answer2, answer3)
     return render_template('quiz_result.html', score=score)
